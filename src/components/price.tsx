db482a27fab7d9e7560b0bed23b71d2fae4fa30a
Konstantin-174 <timokhin_74@hotmail.com>
2022-02-16 20:21:03 +0500

    feat: add styles to video and price layouts

--- src/components/price.tsx
+++ src/components/price.tsx
@@ -3,9 +3,55 @@ import React from 'react';
 export const Price = () => {
     return (
         <section id="price">
-            <h2 className="section-title">
-                Price
-            </h2>
+            <div className="container">
+                <h2 className="price__title">
+                    Price
+                </h2>
+                <div className="layout-3-column">
+                    <div className="price__item">
+                        <h3 className="item__title">Standard</h3>
+                        <p className="item__price">500 $</p>
+                        <div className="item__text">
+                            <p>One location</p>
+                            <p>120 photos in color</p>
+                            <p>12 photos in retouch</p>
+                            <p>Readiness 2-3 weeks</p>
+                            <p>Make up, visage</p>
+                        </div>
+                        <div className="item__button">
+                            <button className="button">Order shooting</button>
+                        </div>
+                    </div>
+                    <div className="price__item">
+                        <h3 className="item__title">Premium</h3>
+                        <p className="item__price">700 $</p>
+                        <div className="item__text">
+                            <p>One or two locations</p>
+                            <p>200 photos in color</p>
+                            <p>20 photos in retouch</p>
+                            <p>Readiness 1-2 weeks</p>
+                            <p>Make up, visage</p>
+                        </div>
+                        <div className="item__button">
+                            <button className="button">Order shooting</button>
+                        </div>
+                    </div>
+                    <div className="price__item">
+                        <h3 className="item__title">Gold</h3>
+                        <p className="item__price">1000 $</p>
+                        <div className="item__text">
+                            <p>Three locations or more</p>
+                            <p>300 photos in color</p>
+                            <p>50 photos in retouch</p>
+                            <p>Readiness 1 week</p>
+                            <p>Make up, visage, hairstyle</p>
+                        </div>
+                        <div className="item__button">
+                            <button className="button">Order shooting</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Player from '../assets/img/video-player.png';
 
 export const Video = () => {
     return (
         <section id="video">
-            <h2 className="section-title">
-                Video
-            </h2>
+            <div className="container">
+                <h2 className="video__title">
+                    Video
+                </h2>
+                <img src={Player} alt="Video player"/>
+            </div>
         </section>
     )
 }
